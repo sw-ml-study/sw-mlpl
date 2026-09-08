edd7482e7a01ba03444a23d790fc6baeee97c85b
Mike Wright <mike@softwarewrighter.com>
2026-09-08 09:07:20 -0700

    deploy pages @ ec462df9

--- mlpl-web-37914a423acb23e1.js
+++ mlpl-web-37914a423acb23e1.js
@@ -995,7 +995,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000005: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("ProgressEvent")], shim_idx: 710, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [NamedExternref("ProgressEvent")], shim_idx: 715, ret: Unit, inner_ret: Some(Unit) }, mutable: true }) -> Externref`.
             const ret = makeMutClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__hab6500415ac99d20);
             return ret;
         },
